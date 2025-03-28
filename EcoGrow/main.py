1e22265cc2756413f8c59a665ab97650a16564db
Dulina77 <dulinawickramasinghe@gmail.com>
2025-03-28 06:08:05 +0530

    Updated Main final to complete the Merge with Chatbot

--- EcoGrow/main.py
+++ EcoGrow/main.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask_cors import CORS
 from space_identification import space_identification_bp  
 from Disease_Prediction import disease_identification_bp
 from plant_prediction import plant_prediction_bp
+from Chatbot.Backend.blueprints.chatbot import chatbot_bp
+from Chatbot.Backend.blueprints.plant import plant_bp
+import subprocess
 
 app = Flask(__name__)
 app.secret_key = "EcoGrow" 
+CORS(app)
 
 app.register_blueprint(space_identification_bp, url_prefix='/space_identification')
 app.register_blueprint(disease_identification_bp, url_prefix = '/disease_identification')
 app.register_blueprint(plant_prediction_bp, url_prefix = '/plant_prediction')
+app.register_blueprint(chatbot_bp,  url_prefix = '/chatbot')
+app.register_blueprint(plant_bp, url_prefix='/plant')
+
+@app.route('/chat')
+def redirect_to_chatbot():
+    return redirect('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
 
 
